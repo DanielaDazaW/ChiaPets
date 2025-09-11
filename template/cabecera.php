b0969eecaa4732acd73b2a60f29c602ee71f38d7
Daniel <dfelipevenegas@ucundinamarca.edu.co>
2025-09-11 13:02:42 -0500

    version0.1

---
+++ template/cabecera.php
@@ -0,0 +1,40 @@
+<?php
+echo "Hola"
+?>
+<!DOCTYPE html>
+<html lang = en>
+<head>
+    <meta charset = "UFT-8">
+    <meta http-equiv= "X-UA-Compatible" content="IE=edge">
+    <meta name="Viewport" content="width=device-width, initial-sacale=1.0">
+    <title>Sitio web </title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css"/>
+</head>
+
+<body>
+    sitioweb
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+        <ul class="nav navbar-nav">
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">CHiaPet</a>
+            </li>
+
+               <li class="nav-item">
+                <a class="nav-link" href="index.php">Inicio</a>
+            </li>
+
+               <li class="nav-item">
+                <a class="nav-link" href="Nosotros.php">Nosotros</a>
+            </li>
+
+               <li class="nav-item">
+                <a class="nav-link" href="campanas.php">Campañas</a>
+            </li>
+        </ul>
+    </nav>
+
+
+    <div class="container">
+        <br/><br/>
+        <div class="row">
