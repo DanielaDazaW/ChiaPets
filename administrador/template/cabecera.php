59d3913340a2726aff5487387908fbed02d0253e
Daniel <dfelipevenegas@ucundinamarca.edu.co>
2025-09-11 18:39:08 -0500

    version0.2

---
+++ administrador/template/cabecera.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+   
+    <link rel="stylesheet" href="./cssa/bootstrap.min.css"/>
+
+     
+</head>
+<body>
+       
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+        <ul class="nav navbar-nav">
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Inicio</a>
+            </li>
+
+               <li class="nav-item">
+                <a class="nav-link" href="index.php">Campañas</a>
+            </li>
+
+               <li class="nav-item">
+                <a class="nav-link" href="Nosotros.php">Mascotas</a>
+            </li>
+
+               <li class="nav-item">
+                <a class="nav-link" href="campanas.php">Servicios</a>
+            </li>
+        </ul>
+    </nav>
+
+
+    <div class="container">
+        <br/><br/>
+        <div class="row">
+
+
+</body>
