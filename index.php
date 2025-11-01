0cf49ce509842cd2cd9a37c04659b48c84578dda
Daniela <danieladazadp18@gmail.com>
2025-11-01 14:21:12 -0500

    version3.0

--- index.php
+++ index.php
@@ -1,7 +1,51 @@
 <?php
 include("template/cabecera.php");
-include("administrador/config/bd.php");
-$stmt = $conexion->prepare("SELECT titulo, descripcion, fecha_inicio, fecha_fin, lugar FROM campanias");
-$stmt->execute();
+?>
+
+
+<!-- Contenedor del logo -->
+<div style="display: flex; justify-content: center; align-items: center; margin-top: 20px;">
+    <img src="img/chiapet3.png" alt="Logo CHIAPET" style="width: 230px; height: 230px;">
+</div>
+
+<!-- Contenedor de los botones -->
+<div style="display: flex; gap: 40px; justify-content: center; align-items: center; margin-top: 50px;">
+
+    <a href="mascotas.php" style="text-decoration: none;">
+        <div style="width: 200px; height: 220px; border-radius: 15px; 
+        box-shadow: 0 4px 8px rgba(0,0,0,0.7); background: #a2e436; 
+        display: flex; flex-direction: column; align-items: center; 
+        justify-content: center; transition: transform 0.2s;">
+            <img src="img/MisMascotas.png" alt="Mascotas" 
+            style="width: 100px; height:100px; margin-bottom: 10px;">
+            <h2 style="color: #333;">Mis Mascotas</h2>
+        </div>
+    </a>
+
+    <a href="campanas.php" style="text-decoration: none;">
+        <div style="width: 200px; height: 220px; border-radius: 15px; 
+        box-shadow: 0 4px 8px rgba(0,0,0,0.7); background: #a2e436; 
+        display: flex; flex-direction: column; align-items: center; 
+        justify-content: center; transition: transform 0.2s;">
+            <img src="img/Campanas.png" alt="Campañas" 
+            style="width: 120px; height:120px; margin-bottom: 10px;">
+            <h2 style="color: #333;">Campañas</h2>
+        </div>
+    </a>
+
+    <a href="reportes_mascota.php" style="text-decoration: none;">
+        <div style="width: 200px; height: 220px; border-radius: 15px; 
+        box-shadow: 0 4px 8px rgba(0,0,0,0.7); background: #a2e436; 
+        display: flex; flex-direction: column; align-items: center; 
+        justify-content: center; transition: transform 0.2s;">
+            <img src="img/reportes.png" alt="Reportes" 
+            style="width: 110px; height:110px; margin-bottom: 10px;">
+            <h2 style="color: #333;">Reportes</h2>
+        </div>
+    </a>
+
+</div>
+
+<?php
 include("template/pie.php");
 ?>
