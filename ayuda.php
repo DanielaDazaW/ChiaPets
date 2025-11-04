bfa204eed1cafd795e58f441c8de3e47e9c9c60f
Daniela <danieladazadp18@gmail.com>
2025-11-04 14:54:22 -0500

    version3.2

--- ayuda.php
+++ ayuda.php
@@ -4,8 +4,11 @@ include("template/cabecera.php");
 
 <div class="container mt-5 text-center">
     <h1>Ayuda y Soporte</h1>
-    <p class="mt-4">En esta sección encontrarás el manual de usuario y respuestas a preguntas frecuentes.<br>
-    El contenido estará disponible próximamente.</p>
+    <p class="mt-4">
+        En esta sección encontrarás el manual de usuario.<br>
+    </p>
+    <!-- Visor PDF incrustado -->
+    <iframe src="img/Manual.pdf" width="80%" height="600px" style="border: none;"></iframe>
 </div>
 
 <?php
